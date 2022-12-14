423ba9de3f2a4b722fbe2a1c1346978f0992a4a1
Artem <temass2012@gmail.com>
2022-12-14 23:28:33 +0300

    привет антон

---
+++ exa5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введи имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "антон")
+{
+
+    Console.WriteLine("УРээээ Антонио");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
